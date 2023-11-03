f829254c142646fbbf988b0d0009b81c65440536
apollolabsdev <apollolabs.bin@gmail.com>
2023-11-03 07:51:58 +0300

    ntp example

---
+++ std_examples/ntp/src/main.rs
@@ -0,0 +1,72 @@
+use anyhow;
+use chrono::{DateTime, Utc};
+use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
+use esp_idf_hal::delay::FreeRtos;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_svc::eventloop::EspSystemEventLoop;
+use esp_idf_svc::nvs::EspDefaultNvsPartition;
+use esp_idf_svc::sntp::{EspSntp, SyncStatus};
+use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
+use std::time::SystemTime;
+
+fn main() -> anyhow::Result<()> {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    let sysloop = EspSystemEventLoop::take()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    let mut wifi = BlockingWifi::wrap(
+        EspWifi::new(peripherals.modem, sysloop.clone(), Some(nvs))?,
+        sysloop,
+    )?;
+
+    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
+        ssid: "Wokwi-GUEST".into(),
+        bssid: None,
+        auth_method: AuthMethod::None,
+        password: "".into(),
+        channel: None,
+    }))?;
+
+    // Start Wifi
+    wifi.start()?;
+
+    // Connect Wifi
+    wifi.connect()?;
+
+    // Wait until the network interface is up
+    wifi.wait_netif_up()?;
+
+    // Print Out Wifi Connection Configuration
+    while !wifi.is_connected().unwrap() {
+        // Get and print connection configuration
+        let config = wifi.get_configuration().unwrap();
+        println!("Waiting for station {:?}", config);
+    }
+
+    println!("Wifi Connected");
+
+    // Create Handle and Configure SNTP
+    let ntp = EspSntp::new_default().unwrap();
+
+    // Synchronize NTP
+    println!("Synchronizing with NTP Server");
+    while ntp.get_sync_status() != SyncStatus::Completed {}
+    println!("Time Sync Completed");
+
+    loop {
+        // Obtain System Time
+        let st_now = SystemTime::now();
+        // Convert to UTC Time
+        let dt_now_utc: DateTime<Utc> = st_now.clone().into();
+        // Format Time String
+        let formatted = format!("{}", dt_now_utc.format("%d/%m/%Y %H:%M:%S"));
+        // Print Time
+        println!("{}", formatted);
+        // Delay
+        FreeRtos::delay_ms(1000);
+    }
+}
